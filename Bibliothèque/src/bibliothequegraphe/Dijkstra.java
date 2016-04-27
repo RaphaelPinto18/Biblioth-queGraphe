2d044d4daa6e621f46de9eafe20f422f3bbd448a
bp818669 <bp818669@IQ-148-11.iut21.u-bourgogne.fr>
2016-04-27 12:59:53 +0200

    Ajout du constructeur

--- Bibliothèque/src/bibliothequegraphe/Dijkstra.java
+++ Bibliothèque/src/bibliothequegraphe/Dijkstra.java
@@ -6,10 +6,27 @@
 
 package bibliothequegraphe;
 
+import bibliothequegraphe.Modèles.Graphe;
+import java.util.ArrayList;
+
 /**
  *
  * @author bp818669
  */
 public class Dijkstra {
+    
+    private ArrayList<Integer> sommets;
+    private ArrayList<Boolean> mark;
+    private ArrayList<Integer> distances;
+    private ArrayList<Integer> antecedents;
+    private Graphe graphe;
+    
+    public Dijkstra(Graphe g){
+        this.sommets = new ArrayList();
+        this.mark = new ArrayList();
+        this.distances = new ArrayList();
+        this.antecedents = new ArrayList();
+        this.graphe = g;
+    }
     
 }
